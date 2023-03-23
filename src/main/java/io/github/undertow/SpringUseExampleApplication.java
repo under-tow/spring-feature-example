13d5ef1d9d2953a01ddf36354b2563398e834202
undertow <1035729652@qq.com>
2023-03-23 22:51:36 +0800

    feat: init

---
+++ src/main/java/io/github/undertow/SpringUseExampleApplication.java
@@ -0,0 +1,12 @@
+package io.github.undertow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUseExampleApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(SpringUseExampleApplication.class, args);
+    }
+
+}
